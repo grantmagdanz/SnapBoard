dce98c145c03abdb93361206ec43d8194a43b062
Grant Magdanz <grantmagdanz@gmail.com>
2015-12-29 14:44:16 -0900

    Basic implementation of autowrapping working with minimal testing.

--- Keyboard/SnapBoard.swift
+++ Keyboard/SnapBoard.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class Snapboard: KeyboardViewController {
+    let CONTEXT_BEFORE_INSERTION_KEY = "contextBefore"
+    let ATTEMPTED_CHARACTER_KEY = "attemptedCharacter"
+    let AUTOWRAP_WAIT_TIME = 0.05
     
     let takeDebugScreenshot: Bool = false
     
@@ -24,14 +27,39 @@ class Snapboard: KeyboardViewController {
         let textDocumentProxy = self.textDocumentProxy
         let keyOutput = key.outputForCase(self.shiftState.uppercase())
         
-        let contextBeforeTextInsertion = textDocumentProxy.documentContextBeforeInput
+        let contextBeforeTextInsertionOpt = textDocumentProxy.documentContextBeforeInput
         
         textDocumentProxy.insertText(keyOutput)
         
         if key.type == .Character || key.type == .SpecialCharacter {
-            let contextAfterTextInsertion = textDocumentProxy.documentContextBeforeInput
+            if let contextBeforeTextInsertion = contextBeforeTextInsertionOpt {
+                
+                // The Snapchat app itself is stopping extra characters from being added. Calling documentContextBeforeInput after a textInsert makes it appear that the character was inserted correctly even if it was not inserted at all (at least from the view of the Snapchat user). So in order to see if we need to autowrap, a brief timer is put onto the call to let Snapchat do its thing and then we test if the character was actually added.
+                NSTimer.scheduledTimerWithTimeInterval(AUTOWRAP_WAIT_TIME, target: self, selector: "autoWrapIfNeeded:", userInfo: [CONTEXT_BEFORE_INSERTION_KEY: contextBeforeTextInsertion, ATTEMPTED_CHARACTER_KEY: keyOutput], repeats: false)
+            }
+        }
+    }
+    
+    /* PRE: This method assumes a few things!
+        1) timer.userInfo is a [String: String]
+        2) timer.userInfo[CONTEXT_BEFORE_INSERTION_KEY] != nil
+        3) timer.userInfo[ATTEMPTED_CHARACTER_KEY] != nil
+    
+        This will crash if any of those are violated
+    
+        This is kind of jenky and is tied to a scheduledTiemrWithTimeInterval call at the time of writing this. It tests if the previous character was infact inserted into the documentProxy and, if not, adds in a new line and the character.
+    
+        Used for autowrapping in Snapchat.
+    */
+    func autoWrapIfNeeded(timer: NSTimer) {
+        let userInfo = timer.userInfo as! [String: String]
+        let contextBeforeTextInsertion = userInfo[CONTEXT_BEFORE_INSERTION_KEY]!
+        
+        if let contextAfterTextInsertion = self.textDocumentProxy.documentContextBeforeInput {
             if contextBeforeTextInsertion == contextAfterTextInsertion {
-                textDocumentProxy.insertText("\u{200B}\n\(keyOutput)")
+                // the character was not added. Add in a new line and the character.
+                let typedCharacter = userInfo[ATTEMPTED_CHARACTER_KEY]!
+                self.textDocumentProxy.insertText("\u{200B}\n\(typedCharacter)")
             }
         }
     }
